882b716d5e7db723b61037e5bdc7bdb47bfcc7fe
Jeff Swisher <jefftswisher@gmail.com>
2019-10-22 22:46:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+  attr_accessor :name
+
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    save  
+  end 
+  
+  def save 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all.each {|x| x}
+  end 
+  
+  def self.print_all
+    binding.pry
+    @@all.each {|x| x(:name)}
+  end 
+  
+  def self.clear_all
+    @@all.clear 
+  end 
+  
+end 
+
